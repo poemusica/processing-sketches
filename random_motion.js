76d08f4e8c4bbb2bb08c5a6fc382346fde311af6
Heather <heatheramahan@gmail.com>
2014-07-09 22:33:30 -0700

    Moved update function into Boid object definition.

--- random_motion.js
+++ random_motion.js
@@ -1,29 +1,51 @@
-// Attaching js code to the canvas by using a sketch object
+// Create a processing sketch object
 var sketch = new Processing.Sketch();
 
 sketch.attachFunction = function(processing) {
-// Global variables go here
+// Global sketch variables
     var blobs = [];
+
+// Coin flip
+function coin_flip(){
+    result = Math.floor(Math.random() * 2);
+    return result;
+}
 
 
 // Definition of Blob object
     function Blob(posX, posY) {
         this.x = posX;
         this.y = posY;
+        this.s = -1.275;
 
-        processing.strokeWeight(3);
-        processing.stroke(0xFFFF3300);
-        processing.fill(0xFFFF5050);
+        this.update = function(){
+            this.x += this.s;
+            this.y += this.s;
+
+            if (this.x < 0 || this.x > processing.width) {
+                this.x = Math.abs(this.x) % processing.width;
+            }
+
+            if (this.y > processing.height){
+                this.y = Math.abs(this.y) % processing.height;
+            }
+
+        };
 
         this.draw = function(){
-            processing.ellipse(this.x, this.y, 15, 15);
+            processing.strokeWeight(3);
+            processing.stroke(0xFFFF3300);
+            processing.fill(0xFFFF5050);
+            processing.ellipse(this.x, this.y, 10, 10);
         };
     }
 
+
+// Populates blobs array.
     function make_blobs(num){
         for (var i = 0; i < num; i++){
-            var x = Math.floor( (Math.random() * 500) + 1 );
-            var y = Math.floor( (Math.random() * 500) + 1 );
+            var x = Math.random() * 500;
+            var y = Math.random() * 500;
             var b = new Blob(x, y);
             blobs.push(b);
         }
@@ -34,9 +56,9 @@ sketch.attachFunction = function(processing) {
         // Set size of canvas
         // MUST BE FIRST LINE OF SETUP FUNCTION!!
         processing.size(500, 500);
-        processing.frameRate(15);
+        processing.frameRate(30);
 
-        // Make array of blobs
+        // Populate blobs array.
         make_blobs(10);
     };
 
@@ -47,18 +69,12 @@ sketch.attachFunction = function(processing) {
 
         for (var i = 0; i < blobs.length; i++) {
             var b = blobs[i];
-            b.x += 1;
-            b.y += 1;
-            if (b.x < 0 || b.x > processing.width) {
-                b.x = Math.abs(b.x) % processing.width;
-            }
-            if (b.y > processing.height){
-                b.y = Math.abs(b.y) % processing.height;
-            }
-            blobs[i].draw();
+            b.update();
+            b.draw();
         }
     };
 };
 
+// Attach sketch to canvas
 var canvas = document.getElementById("mycanv");
 var p = new Processing(canvas, sketch);
