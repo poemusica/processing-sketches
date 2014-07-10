7d9e8d3a4a94d25d1662454906d0ae62c0456a5c
Heather <heatheramahan@gmail.com>
2014-07-10 00:15:01 -0700

    Tidying up code. Fixed screen wrap to work in both directions.

--- random_motion.js
+++ random_motion.js
@@ -1,9 +1,5 @@
 // Create a processing sketch object
-var sketch = new Processing.Sketch();
 
-sketch.attachFunction = function(processing) {
-// Global sketch variables
-    var blobs = [];
 
 // Coin flip
 function coin_flip(){
@@ -11,63 +7,68 @@ function coin_flip(){
     return result;
 }
 
-
 // Definition of Blob object
-    function Blob(posX, posY) {
-        this.x = posX;
-        this.y = posY;
-        this.s = -1.275;
+function Blob(posX, posY, processing){
+    this.x = posX;
+    this.y = posY;
+    this.processing = processing;
+    this.r = Math.random() * 2 * Math.PI;
+    this.s = Math.random() * 2;
+}
 
-        this.update = function(){
-            this.x += this.s;
-            this.y += this.s;
+Blob.prototype.update = function(){
+    this.x += this.s;
+    this.y += this.s;
 
-            if (this.x < 0 || this.x > processing.width) {
-                this.x = Math.abs(this.x) % processing.width;
-            }
-
-            if (this.y > processing.height){
-                this.y = Math.abs(this.y) % processing.height;
-            }
-
-        };
-
-        this.draw = function(){
-            processing.strokeWeight(3);
-            processing.stroke(0xFFFF3300);
-            processing.fill(0xFFFF5050);
-            processing.ellipse(this.x, this.y, 10, 10);
-        };
+    if(this.x < 0) {
+        this.x = this.processing.width;
+    } else if (this.x < 0 || this.x > this.processing.width) {
+        this.x = 0;
     }
 
-
-// Populates blobs array.
-    function make_blobs(num){
-        for (var i = 0; i < num; i++){
-            var x = Math.random() * 500;
-            var y = Math.random() * 500;
-            var b = new Blob(x, y);
-            blobs.push(b);
-        }
+    if(this.y < 0) {
+        this.y = this.processing.height;
+    } else if (this.y > this.processing.height){
+        this.y = 0;
     }
 
-// Setup function
-    processing.setup = function() {
+};
+
+Blob.prototype.draw = function(){
+    this.processing.strokeWeight(3);
+    this.processing.stroke(0xFFFF3300);
+    this.processing.fill(0xFFFF5050);
+    this.processing.ellipse(this.x, this.y, 10, 10);
+};
+
+
+var sketch = new Processing.Sketch();
+
+sketch.attachFunction = function(processing){
+    // Global sketch variables
+    var blobs = [];
+
+    // Setup function
+    processing.setup = function(){
         // Set size of canvas
         // MUST BE FIRST LINE OF SETUP FUNCTION!!
         processing.size(500, 500);
         processing.frameRate(30);
 
         // Populate blobs array.
-        make_blobs(10);
+        for (var i = 0; i < 20; i++){
+            var x = Math.random() * 500;
+            var y = Math.random() * 500;
+            var b = new Blob(x, y, processing);
+            blobs.push(b);
+        }
     };
 
-// Draw function
+    // Draw function
     processing.draw = function() {
-
         processing.background(0xFF33FFCC);
 
-        for (var i = 0; i < blobs.length; i++) {
+        for (var i = 0; i < blobs.length; i++){
             var b = blobs[i];
             b.update();
             b.draw();
